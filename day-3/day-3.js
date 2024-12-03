e2f06970a833b9930f2d3ca355d09740e6d796d8
William Pognonec <william.pognonec@gmail.com>
2024-12-03 01:14:04 -0600

    Refactor

--- day-3/day-3.js
+++ day-3/day-3.js
@@ -1,28 +1,19 @@
-const { log } = require("console")
 const fs = require("fs")
 
 let data = fs.readFileSync("./input.txt", { encoding: "utf8" })
 let regex = /mul\(([0-9]+),([0-9]+)\)|do\(\)|don't\(\)/g
-let muls = data.match(regex)
-
-muls = muls.map((mul) => {
-  if (mul[0] == "m") {
-    return mul
-      .slice(4, -1)
-      .split(",")
-      .map((n) => parseInt(n))
-  }
-  return mul
-})
+let muls = data.matchAll(regex)
 
 let p1 = 0
+let p2 = 0
 let go = true
 
 for (const mul of muls) {
-  if (go && mul[0] != "d") {
-    p1 += mul[0] * mul[1]
+  if (mul[1]) {
+    p1 += mul[1] * mul[2]
+    if (go) p2 += mul[1] * mul[2]
   } else {
-    go = mul == "do()" ? true : false
+    go = mul[0] == "do()" ? true : false
   }
 }
-console.log(p1)
+console.log(p1, p2)
