ef8500294bf3da44df2b05f61986dbc91d8a55b6
William Pognonec <william.pognonec@gmail.com>
2024-12-03 00:04:41 -0600

    Day 3 Part 1

--- day-3/day-3.js
+++ day-3/day-3.js
@@ -1,4 +1,19 @@
+const { log } = require("console")
 const fs = require("fs")
 
 let data = fs.readFileSync("./input.txt", { encoding: "utf8" })
-console.log(data)
+// console.log(data)
+let regex = /mul\(([0-9]+),([0-9]+)\)/g
+let muls = data.match(regex)
+muls = muls.map((mul) =>
+  mul
+    .slice(4, -1)
+    .split(",")
+    .map((n) => parseInt(n))
+)
+
+let p1 = 0
+for (const mul of muls) {
+  p1 += mul[0] * mul[1]
+}
+console.log(p1)
