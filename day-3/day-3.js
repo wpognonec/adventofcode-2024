b5fb3587fab5a6a1e932e77ccb376f40b0bb3f46
William Pognonec <william.pognonec@gmail.com>
2024-12-03 00:25:19 -0600

    Part 2

--- day-3/day-3.js
+++ day-3/day-3.js
@@ -3,17 +3,27 @@ const fs = require("fs")
 
 let data = fs.readFileSync("./input.txt", { encoding: "utf8" })
 // console.log(data)
-let regex = /mul\(([0-9]+),([0-9]+)\)/g
+let regex = /mul\(([0-9]+),([0-9]+)\)|do\(\)|don't\(\)/g
 let muls = data.match(regex)
-muls = muls.map((mul) =>
-  mul
-    .slice(4, -1)
-    .split(",")
-    .map((n) => parseInt(n))
-)
+
+muls = muls.map((mul) => {
+  if (mul[0] == "m") {
+    return mul
+      .slice(4, -1)
+      .split(",")
+      .map((n) => parseInt(n))
+  }
+  return mul
+})
 
 let p1 = 0
+let go = true
+
 for (const mul of muls) {
-  p1 += mul[0] * mul[1]
+  if (go && mul[0] != "d") {
+    p1 += mul[0] * mul[1]
+  }
+  if (mul == "do()") go = true
+  if (mul == "don't()") go = false
 }
 console.log(p1)
