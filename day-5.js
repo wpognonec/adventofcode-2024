296000981fbddb2513682b106e7047222d910754
William Pognonec <william.pognonec@gmail.com>
2024-12-05 15:12:08 -0600

    Refactor

--- day-5.js
+++ day-5.js
@@ -11,10 +11,7 @@ for (let i = 0; i < rules.length; i++) {
   else r[rules[i][1]].push(rules[i][0])
 }
 
-let good = []
-let bad = []
-insts.forEach((inst) => {
-  let bool = true
+function checkInst(inst) {
   for (let i = 1; i < inst.length; i++) {
     let curr = inst[i]
     let arr1 = inst.slice(0, i)
@@ -22,12 +19,28 @@ insts.forEach((inst) => {
     if (
       !(arr1.filter((x) => arr2.includes(x)).toString() === arr1.toString())
     ) {
-      bool = false
-      break
+      return [false, i]
     }
   }
-  if (bool === true) good.push(inst)
+  return [true, 0]
+}
+
+let good = []
+let bad = []
+insts.forEach((inst) => {
+  if (checkInst(inst)[0]) good.push(inst)
   else bad.push(inst)
+})
+
+bad.forEach((inst) => {
+  while (true) {
+    let [b, i] = checkInst(inst)
+    if (!b) {
+      let temp = inst[i]
+      inst[i] = inst[i - 1]
+      inst[i - 1] = temp
+    } else break
+  }
 })
 
 p1 = 0
@@ -35,28 +48,10 @@ good.forEach((inst) => {
   p1 += parseInt(inst[Math.floor(inst.length / 2)])
 })
 
-bad.forEach((inst) => {
-  let bool = false
-  while (!bool) {
-    for (let i = 1; i < inst.length; i++) {
-      let curr = inst[i]
-      let arr1 = inst.slice(0, i)
-      let arr2 = r[curr] ? r[curr] : []
-      if (
-        !(arr1.filter((x) => arr2.includes(x)).toString() === arr1.toString())
-      ) {
-        let temp = inst[i]
-        inst[i] = inst[i - 1]
-        inst[i - 1] = temp
-        break
-      } else if (i === inst.length - 1) bool = true
-    }
-  }
-})
-
 p2 = 0
 bad.forEach((inst) => {
   p2 += parseInt(inst[Math.floor(inst.length / 2)])
 })
+
 console.log(p1)
 console.log(p2)
